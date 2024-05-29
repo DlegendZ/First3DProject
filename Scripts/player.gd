68a4a99bfabdc9265493a1eeccf0d67e104b7c66
DlegendZ <raynaldarvanlim@gmail.com>
2024-05-29 14:42:25 +0800

    latest

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -9,3 +9,12 @@ func _process(delta):
 		apply_torque(Vector3.BACK * delta * force_angular)
 	if Input.is_action_pressed("right") :
 		apply_torque(Vector3.FORWARD * delta * force_angular)
+
+
+
+func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	print("i touch")
+
+
+func _on_body_entered(body):
+	print("i touch")
